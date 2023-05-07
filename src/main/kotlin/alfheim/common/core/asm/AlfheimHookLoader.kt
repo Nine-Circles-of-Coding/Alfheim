1f55ccaf51ad9e9356fc002cf35d7ff21dbafbc2
AlexCatze <alexcatze@gmail.com>
2023-05-07 18:48:28 +0300

    Remove useless check

--- src/main/kotlin/alfheim/common/core/asm/AlfheimHookLoader.kt
+++ src/main/kotlin/alfheim/common/core/asm/AlfheimHookLoader.kt
@@ -14,24 +14,24 @@ import java.io.File
 // -username=AlexSocol
 @MCVersion(value = "1.7.10")
 class AlfheimHookLoader: HookLoader() {
-	
+
 	init {
 		ModInfo.OBF = ASJHookLoader.OBF
-		
+
 		if (!ModInfo.OBF) { // FUCK YOU GRADLE, FUCK YOU INTELLIJ, FUCK YOU ALL
 			FMLRelaunchLog.info("[${ModInfo.MODID.uppercase()}] MoViNg FuCkInG rEsOuRcEs BeCaUsE iDeA iS FUCKED UP!!!")
 			FileUtils.copyDirectory(File("../src/main/resources/"), File("../build/classes/kotlin/main/"))
 		}
-		
+
 		AlfheimConfigHandler.loadConfig(File("config/Alfheim/Alfheim.cfg"))
 	}
-	
+
 	override fun getASMTransformerClass() = arrayOf(AlfheimClassTransformer::class.java.name)
-	
+
 	override fun registerHooks() {
 		FMLRelaunchLog.info("[${ModInfo.MODID.uppercase()}] Loaded coremod. Registering hooks...")
-		
-		
+
+
 		registerHookContainer("alfheim.common.core.asm.hook.AlfheimHookHandler")
 		if (AlfheimConfigHandler.hpHooks) registerHookContainer("alfheim.common.core.asm.hook.AlfheimHPHooks")
 		registerHookContainer("alfheim.common.core.asm.hook.Botania18AndUpBackport")
@@ -53,7 +53,7 @@ class AlfheimHookLoader: HookLoader() {
 		registerHookContainer("alfheim.common.integration.travellersgear.TGHandlerBotaniaAdapterHooks")
 		registerHookContainer("alfheim.common.integration.tinkersconstruct.TraitFairySpawner")
 		registerHookContainer("alfheim.common.item.rod.RedstoneRodHookHandled")
-		
-		if (ModInfo.OBF) ASJASM.registerFieldHookContainer("alfheim.common.core.asm.hook.AlfheimFieldHookHandler")
+        
+        ASJASM.registerFieldHookContainer("alfheim.common.core.asm.hook.AlfheimFieldHookHandler")
 	}
 }
